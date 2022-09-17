05d1749f641d9c3c743aef9d25c23006b475e05e
Nakajima2nd <gp38ate@gmail.com>
2022-09-17 11:02:44 +0900

    あああ

--- frontend/pages/aaa/index.js
+++ frontend/pages/aaa/index.js
@@ -2,7 +2,7 @@ import { Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <Button variant="outlined">aaa</Button>
+    <Button variant="outlined">あああああああ</Button>
   )
 }
 
